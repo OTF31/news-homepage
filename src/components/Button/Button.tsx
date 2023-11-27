416db17e064dcd751e322f6208a79c413f4d7d54
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-11-26 19:11:52 -0500

    add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import type { ButtonProps } from '@mantine/core'
+import { Button as MButton } from '@mantine/core'
+import { ComponentPropsWithoutRef } from 'react'
+
+import classes from './Button.module.css'
+
+type Props = ButtonProps & ComponentPropsWithoutRef<'button'>
+
+const Button = (props: Props) => {
+  const { children, ...rest } = props
+
+  return (
+    <MButton classNames={classes} {...rest}>
+      {children}
+    </MButton>
+  )
+}
+
+export default Button
