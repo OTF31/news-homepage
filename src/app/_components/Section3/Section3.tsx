2313b24b494d61559e2b7edcfccb39ee83eed1d9
Denis Omar Cuyo Ttito <75378049+OTF31@users.noreply.github.com>
2023-11-26 22:28:40 -0500

    add mobile design (#2)

---
+++ src/app/_components/Section3/Section3.tsx
@@ -0,0 +1,50 @@
+import { Box, Group, Image, Stack, Text } from '@mantine/core'
+
+import imageGamingGrowth from '@/assets/images/image-gaming-growth.jpg'
+import imageRetro from '@/assets/images/image-retro-pcs.jpg'
+import imageTopLaptops from '@/assets/images/image-top-laptops.jpg'
+import classes from './Section3.module.css'
+
+const items = [
+  {
+    order: '01',
+    title: 'Reviving Retro PCs',
+    description: 'What happens when old PCs are given modern upgrades?',
+    image: imageRetro.src,
+  },
+  {
+    order: '02',
+    title: 'Top 10 Laptops of 2022',
+    description: 'Our best picks for various needs and budgets.',
+    image: imageTopLaptops.src,
+  },
+  {
+    order: '03',
+    title: 'The Growth of Gaming',
+    description: 'How the pandemic has sparked fresh opportunities.',
+    image: imageGamingGrowth.src,
+  },
+]
+
+const Section3 = () => {
+  return (
+    <Stack className={classes['container']}>
+      {items.map((item, index) => {
+        const { order, title, description } = item
+
+        return (
+          <Group key={index} className={classes['item__container']}>
+            <Image src={item.image} alt={order} />
+            <Box>
+              <Text className={classes['item__order']}>{order}</Text>
+              <Text className={classes['item__title']}>{title}</Text>
+              <Text className={classes['item__description']}>{description}</Text>
+            </Box>
+          </Group>
+        )
+      })}
+    </Stack>
+  )
+}
+
+export default Section3
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-import { AppShell, AppShellMain, Title } from '@mantine/core'
+import { AppShell, AppShellMain, Group, Image } from '@mantine/core'
 
+import logo from '@/assets/images/logo.svg'
+import NavBarButton from './_components/NavBarButton'
+import Section1 from './_components/Section1'
+import Section2 from './_components/Section2'
+import Section3 from './_components/Section3'
 import classes from './page.module.css'
 
 const Home = () => {
   return (
     <AppShell>
       <AppShellMain className={classes.main}>
-        <Title>News homepage</Title>
+        <Group justify='space-between'>
+          <Image src={logo.src} w={45} alt='logo' />
+          <NavBarButton />
+        </Group>
+        <Section1 />
+        <Section2 />
+        <Section3 />
       </AppShellMain>
     </AppShell>
   )
