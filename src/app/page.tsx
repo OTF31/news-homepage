ce55777d201470c0d032caeb4684c43558e483bc
Denis Omar Cuyo Ttito <75378049+OTF31@users.noreply.github.com>
2023-11-26 19:15:28 -0500

    add button component (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { AppShell, AppShellMain, Title } from '@mantine/core'
 
+import classes from './page.module.css'
+
 const Home = () => {
   return (
     <AppShell>
-      <AppShellMain>
+      <AppShellMain className={classes.main}>
         <Title>News homepage</Title>
       </AppShellMain>
     </AppShell>
